de6ba1a29bab3a8e3c1c794982b1aeab24c32e1e
Stan Fu <2016mydream@gmail.com>
2022-05-26 16:44:41 +0200

    update Drone GUI method

--- 5-GUI/test.py
+++ 5-GUI/test.py
@@ -1,82 +1,63 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+from sklearn.datasets import make_regression
+from sklearn.metrics import mean_squared_error
+from mlxtend.evaluate import BootstrapOutOfBag
+from sklearn.model_selection import train_test_split, KFold, LeaveOneOut, ShuffleSplit, cross_validate
+import pandas as pd
+import numpy as np
+import seaborn as sns
 
-"""
-ZetCode PyQt5 tutorial 
+def gen_data(n_samp, n_feat, noise_std, bias, outlier_strength):
+    X, y, true_coef = make_regression(n_samples=n_samp, n_informative=n_feat-1, n_features=n_feat, random_state=43, noise=noise_std,bias=bias, coef=True)
+    true_intercept=bias
+    return X,y,true_coef, true_intercept
 
-This example shows a tooltip on
-a window and a button.
+X, y, true_coef, true_intercept = gen_data(n_samp=300,n_feat=3,noise_std=3.3,bias=0.3, outlier_strength=0)
 
-Author: Jan Bodnar
-Website: zetcode.com
-Last edited: August 2017
-"""
+from sklearn.linear_model import LinearRegression
+class FixedCoefLinearRegression(LinearRegression):
+    def __init__(self, fixed_coef, fixed_intercept):
+        super(FixedCoefLinearRegression, self).__init__()
+        self.fixed_coef = fixed_coef
+        self.fixed_intercept = fixed_intercept
+    def fit(self, X, y, sample_weight=None):
+        self.coef_ = self.fixed_coef
+        self.intercept_ = self.fixed_intercept
+lr_true = FixedCoefLinearRegression(fixed_coef=true_coef, fixed_intercept=true_intercept)
 
-import sys
-from PyQt5.QtWidgets import (QWidget, QToolTip,
-                             QPushButton, QApplication, QMessageBox, QDesktopWidget)
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QFont
+from sklearn.linear_model import LinearRegression
+# Linear Regression
+lr = LinearRegression()
 
+def root_mean_squared_error(y, y_pred):
+    # YOUR CODE HERE
+    sz = len(y.shape)
+    assert sz == 1, "the input data must in 1-dimension"
+    rmse = np.sqrt(mean_squared_error(y, y_pred))
+    return rmse
 
-#
-class Example(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        # 使用initUI()方法创建一个GUI
-        self.initUI()
-
-
-    def initUI(self):
-        # 设置了提示框的字体
-        QToolTip.setFont(QFont('SansSerif', 10))
-
-        # 调用setToolTip（）创建提示框可以使用富文本格式的内容
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        # 创建一个按钮，并为按钮创建了一个提示框
-        btn = QPushButton('Button', self)
-        btn.setToolTip('This is a <b>QPushButton</b> widget')
-        # 调整按钮大小，并且为按钮添加了一个提示框
-        btn.resize(btn.sizeHint())
-        btn.move(50, 50)
+from sklearn.metrics import make_scorer
+rmse_scorer = make_scorer(root_mean_squared_error)
 
 
-        # 创建一个退出按钮
-        qbtn = QPushButton('Quit', self)  # 第二个参数为按钮的父级组件
-        qbtn.clicked.connect(QCoreApplication.instance().quit)
-        qbtn.setToolTip('This is a quit button!')
-        # 调整按钮大小，并且为按钮添加了一个提示框
-        qbtn.resize(btn.sizeHint())
-        qbtn.move(150, 50)
+k_max = 280 #second largest k (max for generating the grid)
+ks = np.array([2,3,5,10])
+ks = np.append(ks,np.arange(15,k_max, 20)) #grid of k values
+ks = np.append(ks,X.shape[0]) #k=n_samples
+print('k or number of splits for the following validation experiments:\n ', ks)
 
-        self.resize(300, 200)  # x, y, wide, high
-        self.center()
-        self.setWindowTitle('Tooltips')  # title
-        self.show()  # 令控件在桌面上显示出来，在内存里创建控件
+re_cf = pd.DataFrame()
+re_rmse = pd.DataFrame()
+for kk in ks:
+    # YOUR CODE HERE
+    cv = ShuffleSplit(n_splits=kk, test_size=.37, random_state=350)
+    c_v = cross_validate(lr, X, y, cv=cv, scoring=rmse_scorer, return_train_score=True)
 
-    def center(self):
-
-        qr = self.frameGeometry()
-        cp = QDesktopWidget().availableGeometry().center()
-        qr.moveCenter(cp)
-        self.move(qr.topLeft())
-
-
-    def closeEvent(self, event):
-        reply = QMessageBox.question(self, 'Message',
-                                     "Are you sure to quit?", QMessageBox.Yes |
-                                     QMessageBox.No, QMessageBox.No)
-
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-
-
-if __name__ == '__main__':
-
-    app = QApplication(sys.argv)  # 创建应用对象，sys.argv是一组命令行参数的列表
-    ex = Example()  # 调用类
-    sys.exit(app.exec_())  # 保证主循环安全退出，exec_防止与python冲突
+    # The given code
+    re_rmse = re_rmse.append(pd.DataFrame({'splits': kk*np.ones(len(c_v['test_score'])),'set': len(c_v['test_score'])*['test'],'model': len(c_v['test_score'])*['lr'],'type': len(c_v['test_score'])*['resampling'] , 'RMSE': c_v['test_score']}),ignore_index=True)
+    re_rmse = re_rmse.append(pd.DataFrame({'splits': kk*np.ones(len(c_v['train_score'])),'set': len(c_v['train_score'])*['train'], 'model': len(c_v['train_score'])*['lr'],'type': len(c_v['train_score'])*['resampling'] , 'RMSE': c_v['train_score']}),ignore_index=True)
+    tmp = pd.DataFrame([true_coef-est.coef_ for est in c_v['estimator']])
+    tmp['diff_intercept'] = [true_intercept-est.intercept_ for est in c_v['estimator']]
+    tmp['splits'] = kk*np.ones((len(tmp)))
+    tmp['type'] = 'resampling'
+    re_cf = re_cf.append(tmp)
+re_cf.columns = ['diff_coef_' + str(i) for i in range(len(true_coef))] + ['diff_intercept'] + ['splits'] + ['type']
