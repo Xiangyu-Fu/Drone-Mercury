d13c74ef5ca6de2fbe8a395636eeb5ae38d561db
fu <fu@fortiss.org>
2022-10-14 16:08:36 +0200

    add the nrf24 testing module

---
+++ 2-Hardware/NRF.py
@@ -0,0 +1,27 @@
+import time
+import serial
+
+# configure the serial connections (the parameters differs on the device you are connecting to)
+ser = serial.Serial(
+    port='/dev/ttyUSB2',
+    baudrate=9600,
+    parity=serial.PARITY_ODD,
+    stopbits=serial.STOPBITS_TWO,
+    bytesize=serial.SEVENBITS
+)
+
+ser.isOpen()
+
+input=1
+while 1 :
+        input = str(int(time.time()%60))
+        print(input)
+        ser.write(input.encode())
+        out = ''
+        # let's wait one second before reading output (let's give device time to answer)
+        time.sleep(1)
+        while ser.inWaiting() > 0:
+            out += ser.read(1)
+
+        if out != '':
+            print (">>" + out)
---
+++ 2-Hardware/NRF/src/main.cpp
@@ -0,0 +1,139 @@
+#if 0
+// SimpleTx - the master or the transmitter
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN  22
+#define CSN_PIN 21
+
+void updateMessage(void);
+void send(void);
+
+const byte slaveAddress[5] = {'R','x','A','A','A'};
+
+
+RF24 radio(CE_PIN, CSN_PIN); // Create a Radio
+
+char dataToSend[10] = "Message 0";
+char txNum = '0';
+
+
+unsigned long currentMillis;
+unsigned long prevMillis;
+unsigned long txIntervalMillis = 1000; // send once per second
+
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Serial.println("SimpleTx Starting");
+
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.setRetries(3,5); // delay, count
+    radio.openWritingPipe(slaveAddress);
+}
+
+//====================
+
+void loop() {
+    currentMillis = millis();
+    if (currentMillis - prevMillis >= txIntervalMillis) {
+        send();
+        prevMillis = millis();
+    }
+}
+
+//====================
+
+void send() {
+
+    bool rslt;
+    rslt = radio.write( &dataToSend, sizeof(dataToSend) );
+        // Always use sizeof() as it gives the size as the number of bytes.
+        // For example if dataToSend was an int sizeof() would correctly return 2
+
+    Serial.print("Data Sent ");
+    Serial.print(dataToSend);
+    if (rslt) {
+        Serial.println("  Acknowledge received");
+        updateMessage();
+    }
+    else {
+        Serial.println("  Tx failed");
+    }
+}
+
+//================
+
+void updateMessage() {
+        // so you can see that new data is being sent
+    txNum += 1;
+    if (txNum > '9') {
+        txNum = '0';
+    }
+    dataToSend[8] = txNum;
+}
+#endif
+
+
+#if 1
+// SimpleRx - the slave or the receiver
+#include <Arduino.h>
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+void getData(void);
+void showData(void);
+
+#define CE_PIN  22
+#define CSN_PIN 21
+
+const byte thisSlaveAddress[5] = {'R','x','A','A','A'};
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+char dataReceived[10]; // this must match dataToSend in the TX
+bool newData = false;
+
+//===========
+
+void setup() {
+
+    Serial.begin(9600);
+
+    Serial.println("SimpleRx Starting");
+    radio.begin();
+    radio.setDataRate( RF24_250KBPS );
+    radio.openReadingPipe(1, thisSlaveAddress);
+    radio.startListening();
+}
+
+//=============
+
+void loop() {
+    getData();
+    showData();
+}
+
+//==============
+
+void getData() {
+    if ( radio.available() ) {
+        radio.read( &dataReceived, sizeof(dataReceived) );
+        newData = true;
+    }
+}
+
+void showData() {
+    if (newData == true) {
+        Serial.print("Data received ");
+        Serial.println(dataReceived);
+        newData = false;
+    }
+}
+#endif
