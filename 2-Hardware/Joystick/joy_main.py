9455088567168bf356f16b382d08a2364493dbcd
Stan Fu <2016mydream@gmail.com>
2022-10-16 23:53:27 +0200

    update some joystick support code

---
+++ 2-Hardware/Joystick/joy_main.py
@@ -0,0 +1,22 @@
+import pygame
+import time
+
+pygame.init()
+pygame.joystick.init()
+done=False
+
+while (done != True):
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+    joystick_count = pygame.joystick.get_count()
+    for i in range(joystick_count):
+        joystick = pygame.joystick.Joystick(i)
+        joystick.init()
+        axes = joystick.get_numaxes()
+        print('================')
+        time.sleep(1)
+        for i in range(axes):
+            axis = joystick.get_axis(i)
+            print(axis)
+            
